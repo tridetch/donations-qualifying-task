a809fa5be218eafb0eb97968015027b77cd99891
tridetch <tridetch@gmail.com>
2022-02-24 02:33:26 +0400

    add functions to contract

--- contracts/Donations.sol
+++ contracts/Donations.sol
@@ -3,21 +3,44 @@ pragma solidity ^0.8.12;
 
 contract Donations {
   string public name = "Donations qualifying task";
-  address payable public owner;
+  address public owner;
 
   mapping(address => uint256) public donations;
 
+  mapping(address => bool) private is_donator;
+  address[] private donatorAddresses;
+
   constructor() {
-    owner = payable(msg.sender);
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner, "Only owner can withdraw funds.");
+    _;
   }
 
   function donate() external payable {
-    donations[msg.sender] = msg.value;
+    donations[msg.sender] += msg.value;
+    if (!is_donator[msg.sender]) {
+      is_donator[msg.sender] = true;
+      donatorAddresses.push(msg.sender);
+    }
   }
 
-  function withdrawAll(address payable withdrawAddress) external {
-    require(msg.sender == owner, "Only owner can withdraw funds.");
-    withdrawAddress.transfer(owner.balance);
-    // add Event
+  function withdraw(address payable withdrawAddress, uint256 amount) external onlyOwner {
+    withdrawAddress.transfer(amount);
+    // emit Event
+  }
+
+  function withdrawAll(address payable withdrawAddress) external onlyOwner {
+    withdrawAddress.transfer(address(this).balance);
+  }
+
+  function allDonators() external view returns (address[] memory) {
+    return donatorAddresses;
+  }
+
+  function balance() external view returns (uint256) {
+    return address(this).balance;
   }
 }
