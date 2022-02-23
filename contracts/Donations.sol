0adb57bbc650cb872c8adc4bd1b205282eb76272
tridetch <tridetch@gmail.com>
2022-02-23 21:38:49 +0400

    create simple donation contract

--- contracts/Donations.sol
+++ contracts/Donations.sol
@@ -1,7 +1,23 @@
-pragma solidity ^0.7.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.12;
 
 contract Donations {
+  string public name = "Donations qualifying task";
+  address payable public owner;
 
-	string public name = "Donations qualifying task";
+  mapping(address => uint256) public donations;
 
+  constructor() {
+    owner = payable(msg.sender);
+  }
+
+  function donate() external payable {
+    donations[msg.sender] = msg.value;
+  }
+
+  function withdrawAll(address payable withdrawAddress) external {
+    require(msg.sender == owner, "Only owner can withdraw funds.");
+    withdrawAddress.transfer(owner.balance);
+    // add Event
+  }
 }
